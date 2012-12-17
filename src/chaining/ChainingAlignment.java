46c89b7f82811705997daf09ea429f77716a334f
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-17 21:47:42 +0900

    かえってきました。これからがんばります

--- src/chaining/ChainingAlignment.java
+++ src/chaining/ChainingAlignment.java
@@ -10,6 +10,40 @@ package chaining;
 public class ChainingAlignment {
 
 	// TODO RedBlackTreeをライブラリの継承で作りたいと思っています
-	// 何かのリストかな
-	RedBlackMultiTree fragments = new RedBlackMultiTree();
+	RedBlackMultiTree<Integer, String> fragments;
+	
+	/*
+	 * 文字列Pの開始位置sxから終了位置ex-1(sx < ex)まで連続した部分文字列を
+	 * P[sx, ex)とする。
+	 * 
+	 * Pはこれからは配列名+List
+	 * 
+	 */
+	/*
+	 * 2本の配列と共にクラスは定義される
+	 * 配列が変わればオブジェクトの再利用は難しい設計とする
+	 */
+	String seqP;
+	String seqQ;
+	
+	/**
+	 * 2つのsequenceと共にこのクラスのオブジェクトインスタンスとオブジェクト参照を生成します。
+	 * @param p
+	 * @param q
+	 */
+	ChainingAlignment(String p, String q){
+		this.seqP = p;
+		this.seqQ = q;
+		this.fragments = new RedBlackMultiTree<Integer, String>();// この型でいいかは微妙
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
