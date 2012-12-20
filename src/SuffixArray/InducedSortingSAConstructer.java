ff341d67183238e9c463e69c5ac58b7ba1894380
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-20 11:39:21 +0900

    constructSAメソッドの提供開始。

--- src/SuffixArray/InducedSortingSAConstructer.java
+++ src/SuffixArray/InducedSortingSAConstructer.java
@@ -27,8 +27,34 @@ public class InducedSortingSAConstructer {
 	 */
 	public static AbstractSuffixArray constructSA(String library) {
 		
+		int[] suffixarray = new int[library.length()];
+		
+//		step 0: Check S or L type.
+		/*
+		 * Sは1, Lは0.
+		 */
+		
+		
+//		step 1: Sort LMS suffixes.
+		
+//		step 2: Sort L-type suffixes.
+		
+//		step 3: Sort S-type suffixes.
 		
 		return null;
+	}
+	
+	
+	/**
+	 * Check "S or L type" in the induced sorting algorithm.
+	 * @param library
+	 * @return
+	 */
+	private byte[] checkSL(String library){
+		byte[] indicator = new byte[library.length()];
+		
+		
+		return indicator;
 	}
 
 }
