f7aaee8256669dad6c12ddb30dfbf4a15861e9e5
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-20 11:31:51 +0900

    inducedSortingの実装開始。仕様策定中。

---
+++ src/SuffixArray/InducedSortingSAConstructer.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package SuffixArray;
+
+/**
+ * @author takashi
+ *	
+ *	inducedSotringを用いてSAを構築します。
+ *	ネストしたメソッドを用いてください。(static only use)
+ *
+ */
+public class InducedSortingSAConstructer {
+
+	/**
+	 * Constructerは用いられません。
+	 */
+	public InducedSortingSAConstructer() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	
+	/**
+	 * indeced sortingによってSAを構築します。
+	 * @param library
+	 * @return
+	 */
+	public static AbstractSuffixArray constructSA(String library) {
+		
+		
+		return null;
+	}
+
+}
---
+++ src/SuffixArray/SAConstructer.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package SuffixArray;
+
+/**
+ * @author takashi
+ *	このinterfaceはSAの構築手法のクラスの仕様を定義します。
+ */
+public interface SAConstructer {
+	public AbstractSuffixArray constructSA(String library);
+}
