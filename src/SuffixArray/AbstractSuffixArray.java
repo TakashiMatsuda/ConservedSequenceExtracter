24c8f85b85af1f61e02e962c37bc897475253b86
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-20 11:09:26 +0900

    SuffixArrayclassのAbstractInterfaceの定義完了、定義開始。searchを必須のメソッドとして定義した。

--- src/SuffixArray/AbstractSuffixArray.java
+++ src/SuffixArray/AbstractSuffixArray.java
@@ -8,5 +8,12 @@ package SuffixArray;
  *
  */
 public interface AbstractSuffixArray {
+	/*
+	 * SuffixArrayがどのようなデータ構造か確認するべき
+	 * ジェネリック型で定義するのが好ましい？
+	 * 
+	 * オーナー文字列と、その接尾辞配列を格納するのがいい
+	 * 
+	 */
 	
 }
