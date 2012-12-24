c535690fb10b329f5d60e40a23c342e1c709fd98
Takashi Matsuda <mazda.takasky@gmail.com>
2012-12-25 00:13:25 +0900

    テストコードを追加しました

---
+++ src/SuffixArray/InducedSortingSAConstructerTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package SuffixArray;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author takashi
+ *
+ */
+public class InducedSortingSAConstructerTest {
+
+	/**
+	 * {@link SuffixArray.InducedSortingSAConstructer#InducedSortingSAConstructer()} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testInducedSortingSAConstructer() {
+		fail("まだ実装されていません");
+	}
+
+	/**
+	 * {@link SuffixArray.InducedSortingSAConstructer#constructSA(java.lang.String)} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testConstructSA() {
+		fail("まだ実装されていません");
+	}
+
+}
